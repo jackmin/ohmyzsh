ce049e14880224d0f2eddc0e82b26f4e8719cb1b
Xiaoyu Min <jackmin@nvidia.com>
2020-11-28 08:03:51 +0800

    fix rsync-repo
    Signed-off-by: Xiaoyu Min <jackmin@nvidia.com>

--- plugins/rsync-repo/rsync-repo.plugin.zsh
+++ plugins/rsync-repo/rsync-repo.plugin.zsh
@@ -33,7 +33,7 @@ function rsync-repo-code-sync {
   [[ ! -d $rebase0 ]] && [[ ! -d $rebase1 ]] &&
   repo_name=`command git rev-parse --show-toplevel` &&
   exclude_file=`command git rev-parse --git-path info/exclude` &&
-  command rsync -rlptzv --progress --delete -e "ssh -o 'ControlPath=$HOME/.ssh/master-socket/%r@%h:%p'" --exclude-from=$exclude_file \
+  command rsync -rlptzv --progress -e "ssh -o 'ControlPath=$HOME/.ssh/master-socket/%r@%h:%p'" --exclude-from=$exclude_file \
     $repo_name $RSYNC_REPO_REMOTE_ROOT/. 2>/dev/null &>! $dir/RSYNC_REPO_LOG )
 }
 
